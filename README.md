b7cfd1f45d51afb43b02e4da76d742b5931bfe9d
chris <chris@DESKTOP-F4DMQ0I>
2017-10-07 09:20:34 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # OpenCV.Net
+
+OpenCV.Net is a reimplementation of [Emgu CV](https://github.com/emgucv/emgucv), a cross-platform .Net wrapper to the OpenCV image processing library.
+
+OpenCV.Net reimplements the methods of the Emgu CV library, but conforms them to the [OpenCV API Reference](http://docs.opencv.org/2.4/modules/refman.html) 
+style so that they can be more easily used with the python/C documentation and examples.
+
+Most notably this means adding return types and optional parameters.
---
+++ src/OpenCV.Net/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("OpenCV.Net")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("OpenCV.Net")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7429735c-e5ca-44a5-8a11-d7cf83064664")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
